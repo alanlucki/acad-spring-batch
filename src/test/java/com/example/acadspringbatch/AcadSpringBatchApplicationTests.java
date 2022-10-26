244d76438c2701c02b6cbfa1d5fb24f61133b165
alan.lucki@gmail.com <alan.lucki@gmail.com>
2022-10-26 15:11:29 -0500

    22/10/26-15:11:29

---
+++ src/test/java/com/example/acadspringbatch/AcadSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acadspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
