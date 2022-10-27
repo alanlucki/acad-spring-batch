14f6eba5a9f8338d3a955121f411415164ac22cf
alan.lucki@gmail.com <alan.lucki@gmail.com>
2022-10-26 21:18:18 -0500

    22/10/26-21:18:18

--- src/main/java/com/example/acadspringbatch/model/Comercio.java
+++ src/main/java/com/example/acadspringbatch/model/Comercio.java
@@ -2,18 +2,32 @@ package com.example.acadspringbatch.model;
 
 public class Comercio {
 
+
     private String contrato;
     private String comercio;
-    private String validacion;
-    
+    private String estado_contr;
+    private String estado_comer;
+    private String ind_180_dias;
+    private String ind_comerc_deb;
+    private String ind_banco_deb;
+
     public Comercio(){
         super();
     }
-    public Comercio(String contrato, String comercio, String validacion) {
+    public Comercio(String contrato, String comercio, String estado_contr, String estado_comer, String ind_180_dias, String ind_comerc_deb, String ind_banco_deb) {
         super();
         this.contrato = contrato;
         this.comercio = comercio;
-        this.validacion = validacion;
+        this.estado_contr = estado_contr;
+        this.estado_comer = estado_comer;
+        this.ind_180_dias = ind_180_dias;
+        this.ind_comerc_deb = ind_comerc_deb;
+        this.ind_banco_deb = ind_banco_deb;
+    }
+
+    @Override
+    public String toString() {
+        return "Comercio{" + "contrato=" + contrato + ", comercio=" + comercio + ", estado_contr=" + estado_contr + ", estado_comer=" + estado_comer + ", ind_180_dias=" + ind_180_dias + ", ind_comerc_deb=" + ind_comerc_deb + ", ind_banco_deb=" + ind_banco_deb + '}';
     }
 
     public String getContrato() {
@@ -32,17 +46,45 @@ public class Comercio {
         this.comercio = comercio;
     }
 
-    public String getValidacion() {
-        return validacion;
+    public String getEstado_contr() {
+        return estado_contr;
     }
 
-    public void setValidacion(String validacion) {
-        this.validacion = validacion;
+    public void setEstado_contr(String estado_contr) {
+        this.estado_contr = estado_contr;
     }
+
+    public String getEstado_comer() {
+        return estado_comer;
+    }
+
+    public void setEstado_comer(String estado_comer) {
+        this.estado_comer = estado_comer;
+    }
+
+    public String getInd_180_dias() {
+        return ind_180_dias;
+    }
+
+    public void setInd_180_dias(String ind_180_dias) {
+        this.ind_180_dias = ind_180_dias;
+    }
+
+    public String getInd_comerc_deb() {
+        return ind_comerc_deb;
+    }
+
+    public void setInd_comerc_deb(String ind_comerc_deb) {
+        this.ind_comerc_deb = ind_comerc_deb;
+    }
+
+    public String getInd_banco_deb() {
+        return ind_banco_deb;
+    }
+
+    public void setInd_banco_deb(String ind_banco_deb) {
+        this.ind_banco_deb = ind_banco_deb;
+    }
+
     
-    @Override
-    public String toString() {
-        return "Comercio [contrato=" + contrato + ", comercio=" + comercio + ", validacion=" + validacion + "]";
-    }
-
 }
--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -2,7 +2,11 @@ DROP TABLE comercio IF EXISTS
 
 CREATE TABLE comercio(
     ID BIGINT IDENTITY NOT NULL PRIMARY KEY,
-    contrato varchar(100) ,
-    comercio varchar(100) ,
-    validacion varchar(100)
+    contrato varchar(20) ,
+    comercio varchar(8) ,
+    estado_contr varchar(2),
+    estado_comer varchar(3),
+    ind_180_dias varchar(1),
+    ind_comerc_deb varchar(1),
+    ind_banco_deb varchar(1)
 );
